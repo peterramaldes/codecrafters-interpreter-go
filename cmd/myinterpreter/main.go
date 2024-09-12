5e2f8e489a39c54d58f995200c4dab82b35a6780
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-09-12 19:50:06 -0300

    Adding Tokenize for parentheses

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -3,7 +3,46 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
+
+type TokenType int
+
+const (
+	EOF TokenType = iota
+	LEFT_PAREN
+	RIGHT_PAREN
+)
+
+func (t TokenType) String() string {
+	switch t {
+	case EOF:
+		return "EOF"
+	case LEFT_PAREN:
+		return "LEFT_PAREN"
+	case RIGHT_PAREN:
+		return "RIGHT_PAREN"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+type Tokenize struct {
+	tokenType TokenType
+
+	// The actual sequence of characters that formed the token
+	// For an EOF token, the lexeme is an empty string.
+	lexeme string
+
+	// The literal value of the token
+	// For most tokens this is null.
+	// For `STRING` and `NUMBER` tokens, it holds the value of the string/number.
+	literal string
+}
+
+func (t Tokenize) String() string {
+	return fmt.Sprintf("%s %s %s", t.tokenType, t.lexeme, t.literal)
+}
 
 func main() {
 	if len(os.Args) < 3 {
@@ -19,14 +58,42 @@ func main() {
 	}
 
 	filename := os.Args[2]
-	fileContents, err := os.ReadFile(filename)
+	content, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
 
-	if len(fileContents) > 0 {
-		panic("Scanner not implemented")
+	var tokens []Tokenize
+	if len(content) > 0 {
+		// Meaning we have data on the file to read into it
+		for i := 0; i < len(content); i++ {
+			c := content[i]
+
+			// Parse token type
+			var tt TokenType
+			switch string(c) {
+			case "(":
+				tt = LEFT_PAREN
+			case ")":
+				tt = RIGHT_PAREN
+			}
+
+			// Parse the lexeme
+			lx := strings.TrimSpace(string(c))
+
+			// Parse literal
+			l := "NULL"
+
+			val := Tokenize{
+				tokenType: tt,
+				lexeme:    lx,
+				literal:   l,
+			}
+
+			fmt.Printf("%v\n", val)
+			tokens = append(tokens, val)
+		}
 	} else {
 		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
 	}
